b7e3f6d3bf8abc5b8ea2afbbba4082eeda87b2f9
Liyang <845974816@qq.com>
2019-07-19 18:41:38 +0800

    波士顿房源问题复习

--- lesson2/myAssignments/lesson2-2.py
+++ lesson2/myAssignments/lesson2-2.py
@@ -1,2 +1,14 @@
-from sklearn.datasets import load_boston
+import matplotlib.pyplot as plt
 
+from sklearn.datasets import load_boston
+data = load_boston()
+X = data['data']
+Y = data['target']
+# print(X)
+# print(Y)
+
+def draw_rm_and_price():
+    plt.scatter(X[:,5], Y)
+
+draw_rm_and_price()
+plt.show()
