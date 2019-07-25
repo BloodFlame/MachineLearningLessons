71730870ca127d0652da998a764338edc5a2ae0b
Liyang <845974816@qq.com>
2019-07-25 18:59:22 +0800

    新思路，按照线路去遍历

--- lesson2/assignments/get_site_connection2.py
+++ lesson2/assignments/get_site_connection2.py
@@ -4,3 +4,20 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+base_url = "https://baike.baidu.com"
+root_href = "/item/%E5%8C%97%E4%BA%AC%E5%9C%B0%E9%93%81/408485"
+
+
+# 从百度百科获取站点Table，返回BeautifulSoup
+def get_table_from_baike(href):
+    kv = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0"}
+    try:
+        r = requests.get(base_url+href, headers = kv, allow_redirects=False)
+    except:
+        print('访问', href, '失败，重试')
+        return get_table_from_baike(href)
+    r.encoding = "utf-8"
+    soup = BeautifulSoup(r.text, 'lxml')
+    return soup.find_all("table")
+
+print(get_table_from_baike(root_href))
