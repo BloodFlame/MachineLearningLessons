f973964996d603c2818c06beeaa11b6f1ff4831b
Liyang <845974816@qq.com>
2019-07-27 19:10:42 +0800

    根据地铁现状修改了下地铁关系，实现了搜索算法，初步设计了按照策略搜索

--- lesson2/assignments/visualize_and_search.py
+++ lesson2/assignments/visualize_and_search.py
@@ -2,12 +2,14 @@
 # 可视化地铁图及完成路径搜索
 import networkx as nx
 import matplotlib.pyplot as plt
+import math
 
 subway_sites_file = open("./subway_sites.txt", "r", encoding="utf8")
 sites_coordinate_file = open("./sites_coordinate.txt", "r", encoding="utf8")
 
 sites_connection = {}
 sites_coordinate = {}
+sites_subway = {}
 
 # 读取站点坐标
 while True:
@@ -24,8 +26,11 @@ while True:
     if not line:
         break
     sites = line.replace('\n', '').split(",")
-    print(sites[0])
+    # print(sites[0])
     for i in range(1, len(sites)):
+        if sites[i] not in sites_subway:
+            sites_subway[sites[i]] = []
+        sites_subway[sites[i]].append(sites[0])
         if sites[i] not in sites_connection:
             sites_connection[sites[i]] = set()
         if i > 1:
@@ -36,8 +41,116 @@ while True:
 subway_sites_file.close()
 sites_coordinate_file.close()
 
-sites_gragh = nx.Graph(sites_connection)
-nx.draw(sites_gragh, sites_coordinate, with_labels=True, node_size=12, font_family='YouYuan', font_size=7)
-plt.show()
+# sites_gragh = nx.Graph(sites_connection)
+# nx.draw(sites_gragh, sites_coordinate, with_labels=True, node_size=12, font_family='YouYuan', font_size=7)
+# plt.show()
+
 
 # 还是很丑,因该是nx对应方法和参数问题,回头再研究,先写搜索方法
+
+def bfs_search(begin, end):
+    pathes = [[begin]]
+    result = []
+    if begin not in sites_connection:
+        return []
+    if end not in sites_connection:
+        return []
+    while pathes:
+        path = pathes.pop(0)
+        if path[-1] not in sites_connection:
+            continue
+        next_sites = sites_connection[path[-1]]
+        for site in next_sites:
+            if site not in sites_connection:
+                continue
+            if site in path:
+                continue
+            if site == end:
+                path.append(site)
+                result.append(path)
+                print(path)
+                continue
+            new_path = path + [site]
+            pathes.append(new_path)
+    return result
+
+
+# 直接搜索不加策略，如果是中间涉及换成，结果会很久很久出不来，但是大多数是没有意义的(绕路了)，需要加策略
+print(bfs_search('北安河站', '焦化厂站'))
+
+
+# 根据经纬度计算距离
+def geo_distance(origin, destination):
+    long1, lat1 = origin
+    long2, lat2 = destination
+    radius = 6371
+    # 两地间弧度计算，经纬度也就是角度值
+    dlat = math.radians(lat2 - lat1)
+    dlon = math.radians(long2 - long1)
+    a = (math.sin(dlat / 2) * math.sin(dlat / 2) +
+         math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) *
+         math.sin(dlon / 2) * math.sin(dlon / 2))
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return radius * c
+
+# 求数组交集，用来判断几个换乘站点是否在同一线路上
+def SINTER(a, b):
+    import collections
+    A, B = map(collections.Counter, (a, b))
+    # collections.Counter & 求两个collections.Counter中相同元素的最小频次
+    return list((A&B).elements)
+
+
+# 判断是否换乘(需要往前两站来判断)
+def if_transfered(cur, prior, pp):
+    a = SINTER(sites_subway[cur], sites_subway[prior])
+    b = SINTER(sites_subway[prior], sites_subway[pp])
+    if SINTER(a, b):
+        return True
+    else:
+        return False
+
+
+# 路程最短或者最少换成或者综合(换乘一次相当于路程增加5km)
+def policy_search(begin, end, policy='shortest'):
+    if policy not in ['shortest', 'least', 'comprehensive']:
+        raise NameError("Undefined policy.")
+    # 这里会定义一个权值 v = k1*距离 + k2*换乘次数
+    # 根据策略不同，k1 和 k2 的权值不同
+    # 搜索过程中超过当前最小值的会被终止
+    distance_k = 0
+    freq_k = 0
+    if policy == 'shortest':
+        distance_k = 1
+    if policy == 'least':
+        freq_k = 1
+    if policy == 'comprehensive':
+        distance_k = 1
+        freq_k = 5
+    min_value = float('inf')
+    pathes = [[begin]]
+    pathes_value = [0]
+    result = []
+    if begin not in sites_connection:
+        return []
+    if end not in sites_connection:
+        return []
+    while pathes:
+        path = pathes.pop(0)
+        path_value = pathes_value.pop(0)
+        if path[-1] not in sites_connection:
+            continue
+        next_sites = sites_connection[path[-1]]
+        for site in next_sites:
+            if site not in sites_connection:
+                continue
+            if site in path:
+                continue
+            if site == end:
+                path.append(site)
+                result.append(path)
+                print(path)
+                continue
+            new_path = path + [site]
+            pathes.append(new_path)
+    return result
